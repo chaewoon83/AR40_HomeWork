6cbc96529dfe8b7bef468c0e3481545c80321fbb
채운 김 <balexgood@gmail.com>
2022-01-18 14:22:53 +0900

    pointer study 추가

---
+++ PointerStudy/PointerStudy.cpp
@@ -0,0 +1,28 @@
+﻿
+
+//void Heal(int _Hp) 
+//{
+//    _Hp += 10;
+//}
+
+void Heal(int* _Hp)
+{
+    *_Hp += 10;
+}
+
+// 
+
+//int Heal(Image* _Hp)
+//{
+//    _Hp += 10;
+//    return _Hp;
+//}
+
+int main()
+{
+    int PlayerHp = 0;
+
+    int* Ptr = &PlayerHp;
+    Heal(&PlayerHp);
+    int a = 0;
+}
