b89cbdcb64ab781e3f036aaf6729b74ec598f244
chaewoon83 <balexgood@gmail.com>
2022-01-21 22:03:13 +0900

    텍스트rpg

--- HomeWork20220120/HomeWork20220120.cpp
+++ HomeWork20220120/HomeWork20220120.cpp
@@ -6,17 +6,17 @@
 class Player
 {
 public:
-    __int64 ex1;
-    int Hp;
+    char ex1;
+    char Hp;
     int Att;
-    char ex;
-    __int64 Att1;
-    char Hp1;
-    int Att2;
-    char Name[10];
-    char Name1;
-    int Att3[10];
-    char Name2;
+    __int64 ex;
+    char Att1;
+    //char hp1;
+    //int att2;
+    //char name[10];
+    //char name1;
+    //int att3[10];
+    //char name2;
 };
 class Ex1
 {
@@ -29,16 +29,17 @@ int main()
     Player NewPlayer = Player();
     Ex1 NewEx1 = Ex1();
     __int64 HpPtr = (__int64)&NewPlayer.ex1;
-    __int64 HpPtr0 = (__int64)&NewPlayer.Hp;
-    __int64 HpPtr1 = (__int64)&NewPlayer.Att;
-    __int64 HpPtr1_2 = (__int64)&NewPlayer.ex;
-    __int64 HpPtr2 = (__int64)&NewPlayer.Att1;
-    __int64 HpPtr3 = (__int64)&NewPlayer.Hp1;
-    __int64 HpPtr4 = (__int64)&NewPlayer.Att2;
-    __int64 HpPtr5 = (__int64)&NewPlayer.Name;
-    __int64 HpPtr6 = (__int64)&NewPlayer.Name1;
-    __int64 HpPtr7 = (__int64)&NewPlayer.Att3;
-    __int64 HpPtr8 = (__int64)&NewPlayer.Name2;
+    __int64 HpPtr0 = (__int64)&NewPlayer.Hp - HpPtr;
+    __int64 HpPtr1 = (__int64)&NewPlayer.Att - HpPtr;
+    __int64 HpPtr1_2 = (__int64)&NewPlayer.ex - HpPtr;
+    __int64 HpPtr2 = (__int64)&NewPlayer.Att1 - HpPtr;
+    int PlayerSize = sizeof(NewPlayer);
+    //__int64 HpPtr3 = (__int64)&NewPlayer.Hp1;
+    //__int64 HpPtr4 = (__int64)&NewPlayer.Att2;
+    //__int64 HpPtr5 = (__int64)&NewPlayer.Name;
+    //__int64 HpPtr6 = (__int64)&NewPlayer.Name1;
+    //__int64 HpPtr7 = (__int64)&NewPlayer.Att3;
+    //__int64 HpPtr8 = (__int64)&NewPlayer.Name2;
     int Ex1Size = sizeof(NewEx1);
 
     int a = 0;
